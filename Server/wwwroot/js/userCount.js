0ca8e7bd4dc0ba3922c6af5b16f9c702c9e781d0
Mishu <54718081+Misho12345@users.noreply.github.com>
2023-10-17 20:22:26 +0300

    Setup signalr

---
+++ Server/wwwroot/js/userCount.js
@@ -0,0 +1,21 @@
+﻿var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+function fulfilled() {
+    console.log("Successful connection");
+    newWindowLoadedOnClient();
+}
+
+function rejected() {
+    console.log("Unsuccessful connection");
+}
+
+connectionUserCount.start().then(fulfilled, rejected)
